cb9c3276fcd70352905cd94a854aab3738277f20
Leonardo Lazzaro <llazzaro@dc.uba.ar>
2015-09-25 01:36:24 -0300

    move baseDAM

---
+++ analyzer/dam/__init__.py
@@ -0,0 +1,33 @@
+import abc
+
+
+class BaseDAM(object):
+    ''' base class for DAO '''
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self):
+        self.__symbol = None
+
+    def read_quotes(self, start, end):
+        raise NotImplementedError('Abstract method called')
+
+    def write_quotes(self, quotes):
+        raise NotImplementedError('Abstract method called')
+
+    def read_ticks(self, start, end):
+        raise NotImplementedError('Abstract method called')
+
+    def write_ticks(self, ticks):
+        raise NotImplementedError('Abstract method called')
+
+    def read_fundamental(self):
+        raise NotImplementedError('Abstract method called')
+
+    def write_fundamental(self, keyTimeValueDict):
+        raise NotImplementedError('Abstract method called')
+
+    def setup(self, settings):
+        raise NotImplementedError('Abstract method called')
+
+    def commit(self):
+        raise NotImplementedError('Abstract method called')
