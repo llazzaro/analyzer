fa18fc997a33e1c76c19420cc9874be4bc011490
Leonardo Lazzaro <llazzaro@dc.uba.ar>
2015-09-26 00:28:16 -0300

    trading center refactor

--- analyzer/backtest/trading_center.py
+++ analyzer/backtest/trading_center.py
@@ -3,12 +3,13 @@ Created on Dec 18, 2011
 
 @author: ppa
 '''
-import time
-
 import logging
 
-from pyStock.models import Order
-from analyzer.lib.errors import Errors, UfException
+from pyStock.models import (
+    Order,
+    Security,
+    FillOrderStage,
+)
 
 LOG=logging.getLogger()
 
@@ -18,110 +19,74 @@ class TradingCenter(object):
     trading center
     Note: set metricNames before adding accounts
     '''
-    def __init__(self):
-        ''' constructor '''
-        self.accountManager=None
-        self.__updatedOrder={}  # SAMPLE {"EBAY": [order1, order2]}
-        self.__placedOrder={}  # SAMPLE {"EBAY": [order1, order2]}
-        self.__lastTickDict=None
+    def __init__(self, session):
+        self.session = session
+        self.updated_order={}  # SAMPLE {"EBAY": [order1, order2]}
+        self.placed_order={}  # SAMPLE {"EBAY": [order1, order2]}
+        self.last_tick_dict=None
 
     def updated_order(self):
-        ''' return orders with status changes '''
         updatedOrder={}
-        updatedOrder.update(self.__updatedOrder)
-        self.__updatedOrder.clear()
+        updatedOrder.update(self.updated_order)
+        self.updated_order.clear()
 
         return updatedOrder
 
     def placed_order(self):
-        ''' return orders that has been placed '''
         placedOrder={}
-        placedOrder.update(self.__placedOrder)
-        self.__placedOrder.clear()
+        placedOrder.update(self.placed_order)
+        self.placed_order.clear()
 
         return placedOrder
 
-    def cancel_order(self, symbol, orderId):
-        ''' cancel an order '''
-        if symbol not in self.__openOrders:
-            LOG.warn("Can't cancel order %s because there is no open orders for symbol %s" % (orderId, symbol))
+    def cancel_order(self, order):
+        if order not in self.open_orders:
+            LOG.warn("Can't cancel order %s because there is no open orders for symbol {0}".format(order.security.symbol))
             return
 
-        if orderId not in self.open_orders(symbol):
-            LOG.warn("Can't cancel order %s because there is no open orders for order id %s with symbol %s" % (orderId, orderId, symbol))
-            return
-
-        # TODO cancel the order and update history
-        del self.__openOrders[symbol][orderId]
-
-        # if no open orders left for that symbol, remove it
-        if not len(self.__openOrders[symbol]):
-            del self.__openOrders[symbol]
-
-        LOG.debug("Order canceled: %s" % orderId)
+        order.cancel()
+        LOG.debug("Order canceled: {0}" .format(order.pk))
 
     def cancel_all_open_orders(self):
-        ''' cancel all open order '''
-        for symbol, orderIdAndOrderDict in self.__openOrders.items():
-            for orderId, order in orderIdAndOrderDict.values():
-                order.status=Order.CANCELED  # change order state
-                self.__closedOrders[orderId]=order
+        for order in self.open_orders:
+            order.cancel()
 
-            del self.__openOrders[symbol]
+    def consume_ticks(self, raw_tick):
+        self._check_and_execute_orders(raw_tick)
 
-    def consume_ticks(self, tickDict):
-        ''' consume ticks '''
-        self._checkAndExecuteOrders(tickDict)
-        self.accountManager.updateAccountsPosition(tickDict)
-
-    def _check_and_execute_orders(self, tickDict):
-        ''' check and execute open order '''
-        self.__lastTickDict=tickDict
-        for symbol, tick in tickDict.iteritems():
-            LOG.debug("_checkAndExecuteOrders symbol %s with tick %s, price %s" % (symbol, tick.time, tick.close))
-            if symbol not in self.__openOrders:
-                LOG.debug("_checkAndExecuteOrders no open orders for symbol %s with tick %s, price %s" % (symbol, tick.time, tick.close))
+    def _check_and_execute_orders(self, raw_tick):
+        self.last_tick_dict=raw_tick
+        for symbol, tick in raw_tick.iteritems():
+            LOG.debug("_check_and_execute_orders symbol %s with tick %s, price %s" % (symbol, tick.time, tick.close))
+            if symbol not in self.open_orders:
+                LOG.debug("_check_and_execute_orders no open orders for symbol %s with tick %s, price %s" % (symbol, tick.time, tick.close))
                 continue
 
-            for order in self.__openOrders[symbol].values():
-                if self.isOrderMet(tick, order):
-                    self.__executeOrder(tick, order)
+            for order in self.open_orders[symbol].values():
+                if order.is_order_met(tick):
+                    self._execute_order(tick, order)
 
-    def __checkAndExecuteOrder(self, order):
-        ''' check and execute one order '''
-        tick=self.__lastTickDict.get(order.symbol)
+    def _check_and_execute_order(self, order):
+        tick=self.last_tick_dict.get(order.symbol)
         if tick is None:
-            LOG.debug("_checkAndExecuteOrder no open orders for symbol %s with tick %s, price %s" % (order.symbol, tick.time, tick.close))
+            LOG.debug("_check_and_execute_order no open orders for symbol %s with tick %s, price %s" % (order.symbol, tick.time, tick.close))
             return
 
-        if self.isOrderMet(tick, order):
-            self.__executeOrder(tick, order)
+        if order.is_order_met(tick):
+            self._execute_order(tick, order)
 
-    def __execute_order(self, tick, order):
-        ''' execute an order '''
-        account=self.accountManager.getAccount(order.accountId)
-        if not account:
-            raise UfException(Errors.INVALID_ACCOUNT,
-                              ''' Account is invalid with accountId %s for order %s''' % (order.accountId, order.orderId))
-        else:
-            LOG.debug("executing order %s" % order)
-            try:
-                account.execute(order, tick)
-                order.status=Order.FILLED
-                order.filledTime=time.time()
-
-                self.__closedOrders[order.orderId]=order
-                self.__updatedOrder[order.orderId]=order
-            except Exception as ex:
-                LOG.error("Got exception when executing order %s: %s" % (order, ex))
-
-            del self.__openOrders[order.symbol][order.orderId]
-            if not len(self.__openOrders[order.symbol]):
-                del self.__openOrders[order.symbol]
-
-    def is_order_met(self, tick, order):
-        ''' whether order can be execute or not '''
-        return order.is_order_met(tick)
+    def _execute_order(self, tick, order):
+        account=order.account
+        LOG.debug("executing order {0}".format(order))
+        account.execute(order, tick)
+        order.update_stage(FillOrderStage())
 
     def open_order_by_id(self, id):
-        raise NotImplementedError()
+        return self.session.query(Order).filter_by(id=id).first()
+
+    def open_orders_by_symbol(self, symbol):
+        return self.session.query(Order).join(Security).filter_by(symbol=symbol)
+
+    @property
+    def open_orders(self):
+        return self.session.query(Order).all()
--- tests/unit/test_trading_center.py
+++ tests/unit/test_trading_center.py
@@ -4,27 +4,52 @@ Created on Dec 18, 2011
 @author: ppa
 '''
 import unittest
+from datetime import datetime
+
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm.session import Session
+
 from analyzer.backtest.trading_center import TradingCenter
+from pyStock import Base
+from pyStock.models.money import Currency, Money
 from pyStock.models import (
     Stock,
     Tick,
     BuyOrder,
-    SellOrder,
     CancelOrderStage,
     Account,
+    Broker,
+    Owner,
+    Exchange,
 )
-from analyzer.lib.errors import UfException
+
+
+# Connect to the database and create the schema within a transaction
+engine = create_engine('sqlite://')
+connection = engine.connect()
+transaction = connection.begin()
+Base.metadata.create_all(connection)
 
 
 class testTradingCenter(unittest.TestCase):
+
     def setUp(self):
-        pass
+        self.trans = connection.begin()
+        self.session = Session(connection)
+
+        currency = Currency(name='Pesos', code='ARG')
+        self.exchange = Exchange(name='Merval', code='MERV', currency=currency)
+        self.owner = Owner(name='poor owner')
+        self.broker = Broker(name='broker1')
+        self.account = Account(owner=self.owner, broker=self.broker)
+        self.account.deposit(Money(amount=10000, currency=currency))
 
     def tearDown(self):
-        pass
+        self.trans.rollback()
+        self.session.close()
 
     def testGetCopyAccounts(self):
-        tc=TradingCenter()
+        tc=TradingCenter(self.session)
         tc.createAccountWithMetrix(100000, 0)
         tc.createAccountWithMetrix(200000, 0)
 
@@ -33,44 +58,21 @@ class testTradingCenter(unittest.TestCase):
         self.assertEquals(2, len(accounts))
 
     def testGetCopyAccount(self):
-        tc=TradingCenter()
+        tc=TradingCenter(self.session)
         accountId1=tc.createAccountWithMetrix(100000, 0)
 
         account=tc.getCopyAccount(accountId1)
-        print(account)
         self.assertEquals(100000, account.cash)
 
-    def testIsOrderMet(self):
-        tc=TradingCenter()
-        tick1=Tick('time', 'open', 'high', 'low', 13.20, 'volume')
-        order1=BuyOrder(accountId=None, symbol='symbol', price=13.25, share=10)
-        order2=BuyOrder(accountId=None, symbol='symbol', price=13.15, share=10)
-        order3=SellOrder(accountId=None, symbol='symbol', price=13.25, share=10)
-        order4=SellOrder(accountId=None, symbol='symbol', price=13.15, share=10)
-
-        self.assertEquals(True, tc.isOrderMet(tick1, order1))
-        self.assertEquals(False, tc.isOrderMet(tick1, order2))
-        self.assertEquals(False, tc.isOrderMet(tick1, order3))
-        self.assertEquals(True, tc.isOrderMet(tick1, order4))
-
-    def testValidOrder(self):
-        """
-            Order instantiation raises exception if it is not valid
-        """
-
-        account=Account()
-        unknown_acc=Account()
-        stock=Stock(symbol='symbol')
-        BuyOrder(account=account, security=stock, price=13.25, share=10)
-        BuyOrder(account=unknown_acc, security=stock, price=13.25, share=10)
-
     def test_open_orders_by_order_id(self):
-        account = Account()
-        stock=Stock(symbol='symbol')
-        order1=BuyOrder(account=account, security=stock, price=13.2, share=10)
-        BuyOrder(account=account, security=stock, price=13.25, share=10)
+        stock=Stock(symbol='symbol', description='a stock', ISIN='US123456789', exchange=self.exchange)
+        order1=BuyOrder(account=self.account, security=stock, price=13.2, share=10)
+        order2=BuyOrder(account=self.account, security=stock, price=13.25, share=10)
+        self.session.add(order1)
+        self.session.add(order2)
+        self.session.commit()
 
-        tc=TradingCenter()
+        tc=TradingCenter(self.session)
         order=tc.open_order_by_id(order1.id)
         self.assertEquals(order1, order)
 
@@ -79,50 +81,52 @@ class testTradingCenter(unittest.TestCase):
 
     def testGetOpenOrdersBySymbol(self):
 
-        account = Account()
-        stock=Stock(symbol='symbol')
-        order1=BuyOrder(account=account, security=stock, price=13.2, share=10)
-        order2=BuyOrder(account=account, security=stock, price=13.25, share=10)
+        stock=Stock(symbol='symbol', description='a stock', ISIN='US123456789', exchange=self.exchange)
+        order1=BuyOrder(account=self.account, security=stock, price=13.2, share=10)
+        order2=BuyOrder(account=self.account, security=stock, price=13.25, share=10)
+        self.session.add(order1)
+        self.session.add(order2)
+        self.session.commit()
 
-        tc=TradingCenter()
-        orders=tc.open_orders_by_symbol('symbol1')
-        self.assertEquals([order1, order2], orders)
+        tc=TradingCenter(self.session)
+        orders=tc.open_orders_by_symbol('symbol')
+        self.assertEquals([order1, order2], list(orders))
 
     def testCancelOrder(self):
 
-        account = Account()
-        stock=Stock(symbol='symbol')
-        order1=BuyOrder(account=account, symbol=stock, price=13.2, share=10)
-        order2=BuyOrder(account=account, symbol=stock, price=13.25, share=10)
+        stock=Stock(symbol='symbol', description='a stock', ISIN='US123456789', exchange=self.exchange)
+        order1=BuyOrder(account=self.account, security=stock, price=13.2, share=10)
+        order2=BuyOrder(account=self.account, security=stock, price=13.25, share=10)
 
+        self.session.add(order1)
+        self.session.add(order2)
+        self.session.commit()
 
-        tc=TradingCenter()
+        tc=TradingCenter(self.session)
 
-        tc.cancelOrder('id1')
-        print(tc._TradingCenter__openOrders)
-        print(tc._TradingCenter__closedOrders)
-        self.assertEquals({'symbol1': [order2]}, tc._TradingCenter__openOrders)
-        self.assertEquals({'id1': order1}, tc._TradingCenter__closedOrders)
+        order1.cancel()
+        self.assertEquals([order2], tc.open_orders)
+        self.assertEquals([order1], tc.closed_orders)
         self.assertEquals(CancelOrderStage, type(order1.current_stage))
 
-        tc.cancelOrder('id2')
-        print(tc._TradingCenter__openOrders)
-        print(tc._TradingCenter__closedOrders)
-        self.assertEquals({}, tc._TradingCenter__openOrders)
-        self.assertEquals({'id1': order1, 'id2': order2}, tc._TradingCenter__closedOrders)
+        tc.cancelorder(order2)
+        self.assertEquals({}, tc.open_orders())
+        self.assertEquals([order1, order2], tc.closed_orders)
 
     def testCancelAllOpenOrders(self):
-        order1=BuyOrder(accountId='accountId', symbol='symbol1', price=13.2, share=10, orderId='id1')
-        order2=BuyOrder(accountId='accountId', symbol='symbol1', price=13.25, share=10, orderId='id2')
+        security=Stock(symbol='symbol', description='a stock', ISIN='US123456789', exchange=self.exchange)
+        order1=BuyOrder(account=self.account, security=security, price=13.2, share=10)
+        order2=BuyOrder(account=self.account, security=security, price=13.25, share=10)
 
-        tc=TradingCenter()
-        tc._TradingCenter__openOrders={'symbol1': [order1, order2]}
+        self.session.add(order1)
+        self.session.add(order2)
+        self.session.commit()
 
-        tc.cancelAllOpenOrders()
-        print(tc._TradingCenter__openOrders)
-        print(tc._TradingCenter__closedOrders)
-        self.assertEquals({}, tc._TradingCenter__openOrders)
-        self.assertEquals({'id1': order1, 'id2': order2}, tc._TradingCenter__closedOrders)
+        tc=TradingCenter(self.session)
+
+        tc.cancel_all_open_orders()
+
+        self.assertEquals([], tc.open_order())
 
     def testConsume(self):
         pass
