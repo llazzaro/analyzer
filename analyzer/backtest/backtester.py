e11a12dfeac277acf5a56bf42ce993f958e30c26
Leonardo Lazzaro <llazzaro@dc.uba.ar>
2015-11-25 01:44:11 -0300

    new backtester code

--- analyzer/backtest/backtester.py
+++ analyzer/backtest/backtester.py
@@ -1,6 +1,15 @@
 import logging
 
-LOG=logging.getLogger(__name__)
+from analyzer.constant import (
+    BUY,
+    SELL,
+)
+from pyStock.models import (
+    BuyOrder,
+    SellOrder,
+)
+
+log=logging.getLogger(__name__)
 
 
 class BackTester(object):
@@ -9,27 +18,37 @@ class BackTester(object):
         and execute strategies
         broadcast actions for backtesting
     '''
-    def __init__(self, store, pubsub, securities, start=None, end=None):
+    def __init__(self, session, account, store, pubsub, security, strategy, start=None, end=None):
         self.store = store
+        self.account = account
+        self.session = session
+        self.strategy = strategy
         self.pubsub = pubsub
-        self.securities = securities
+        self.security = security
         self.start = start
         self.end = end
 
     def _retrieve_ticks(self, security, start, end):
-        pass
+        return self.store[security.exchange.code].read(security.symbol)
 
-    def execute(self, tick):
-        action = self.strategy.update(tick)
-        if action:
-            action.is_backtest = True
-            self.pubsub.publish('action', action)
+    def execute(self, action):
+        # back tester buy and sells everything
+        if action.action() == BUY:
+            order = BuyOrder(account=self.account, security=self.security, price=action.price(), share=self.calculate_buy_share(security))
+        if action.action() == SELL:
+            order = SellOrder(account=self.account, security=self.security, price=action.price(), share=self.calculate_sellshare(security))
+
+        self.session.add(order)
+        self.session.commit()
 
     def consume(self):
-        for security in self.securities:
-            for tick in self._retrieve_ticks(security, self.start, self.end):
-                LOG.info('New tick {0}'.format(tick))
-                # strategy will create actions
-                # traging center will see the actions
-                # and will place orders
-                self.execute(tick)
+        log.info('Processing security {0}'.format(self.security))
+        # strategy will create actions
+        # traging center will see the actions
+        # and will place orders
+        data_frame = self._retrieve_ticks(self.security, self.start, self.end).data
+        for action in self.strategy.update(security, data_frame):
+            if action:
+                action.is_backtest = True
+                self.execute(action)
+                self.pubsub.publish('actions', action.__json__())
